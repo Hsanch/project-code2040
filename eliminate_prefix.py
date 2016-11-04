87b6940f31f2035ecf5996757467e4362454a3ba
Hermes Sanchez <hermes.sanchez@assumption.edu>
2016-11-04 13:56:11 -0400

    added a working version of the eliminate_prefix.py program

--- eliminate_prefix.py
+++ eliminate_prefix.py
@@ -2,6 +2,16 @@ import json
 import requests
 
 def eliminate_prefix(Prefix, Array):
+	index= 0
+	while index < len(Array):
+		tempString = Array[index]
+		if tempString.find(Prefix) != -1:
+			Array.remove(Array[index])
+		else:
+			index+=1
+	return Array
+
+def eliminate_prefix2(Prefix, Array):
 	index = 0
 	temp_ary = Array
 	while index < len(temp_ary):
@@ -18,40 +28,43 @@ with requests.Session() as c:
 	
 	prefix_data = {
 		
-		'token' : '297b6feab3721bf2c68527a718b620f4'
+		'token':'297b6feab3721bf2c68527a718b620f4'
 	}
 
 	headers = { 'Content-Type' : 'application/json'}
 
 	response = c.post(url, data = json.dumps(prefix_data), headers = headers)
 	print response.content
-	content = json.loads(response.content)
 
-print response
+content = json.loads(response.content)
+print content
 
 prefix = content['prefix']
 array = content['array']
 
 
-new_array =  eliminate_prefix(prefix, array)
-new_array = json.dumps(new_array)
 
-
-print new_array
+array =  eliminate_prefix(prefix,array)
 
 with requests.Session() as j: 
+
+	array = eliminate_prefix(prefix,array)
+	#array = json.dumps(array)
+
+	token = '297b6feab3721bf2c68527a718b620f4'
+	#token = json.dumps(token)
 
 	url = 'http://challenge.code2040.org/api/prefix/validate'
 
 	new_data = {
 
-		'token' : '297b6feab3721bf2c68527a718b620f4',
-		'array' : new_array
+		'token': token,
+		'array': array
 	}
-
 	headers = {'Content-Type' : 'application/json'}
+	response2 = j.post(url, json = new_data, headers = headers)
 
-	response2 = j.post(url, json = json.dumps(new_data), headers = headers)
 
+print "\n\n"
 print response2
 print new_data
